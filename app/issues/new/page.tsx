4d14c20cc624c45ecad8f395a4d5fde7179af57b
Augah2001 <Augahmups4@gmail.com>
2024-02-19 23:02:21 +0200

    react-hook-form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,16 +4,31 @@ import { Button, TextArea, TextField } from "@radix-ui/themes";
 import React from "react";
 import "easymde/dist/easymde.min.css";
 import SimpleMdeReact from "react-simplemde-editor";
+import {useForm,Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import {z} from "zod";
 
 const NewIssue = () => {
+
+  const router = useRouter();
+  const { register, handleSubmit, control } = useForm();
   return (
-    <div className="max-w-xl space-y-5">
+    <form className="max-w-xl space-y-5" onSubmit={handleSubmit((data)=> {
+      axios.post("http://localhost:3000/api/issues",data).then((res)=> router.push('/issues') ).catch(err=> console.log(err))
+    })}>
       <TextField.Root>
-        <TextField.Input placeholder="title" />
+        <TextField.Input placeholder="title" {...register('title')}/>
       </TextField.Root>
-      <SimpleMdeReact placeholder="description" />
+
+      <Controller
+        name="description"
+        control={control}
+        render = {({field})=> <SimpleMdeReact placeholder="description" {...field}/> }
+      />
+      
       <Button>Submit new issue</Button>
-    </div>
+    </form>
   );
 };
 
